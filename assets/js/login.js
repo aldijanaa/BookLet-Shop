b2f67b6eae13fddd6088af26b7f6c6984b03cb61
aldijanaa <aldijana.culezovic@stu.ibu.edu.ba>
2024-03-17 17:11:37 +0100

    fixing main

---
+++ assets/js/login.js
@@ -0,0 +1,19 @@
+/*function validateForm() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    
+    if (email.trim() === "") {
+        alert("Please enter your email");
+        return false;
+    }
+    
+    if (password.trim() === "") {
+        alert("Please enter your password");
+        return false;
+    }
+
+    // Redirecting to index.html
+    window.location.href = "index.html";
+    
+    return true; // Form submission will proceed if all fields are filled
+}*/
---
+++ assets/js/script.js
@@ -0,0 +1,104 @@
+'use strict';
+
+
+/**
+ * add event on elements
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+    if (elem.length > 1) {
+      for (let i = 0; i < elem.length; i++) {
+        elem[i].addEventListener(type, callback);
+      }
+    } else {
+      elem.addEventListener(type, callback);
+    }
+  }
+  
+  /**
+ * navbar toogle -- ovo da postane navbar aktivan
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+/**
+ * active header & back top btn when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const activeElemOnScroll = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", activeElemOnScroll);
+
+/*filter function in POPULAR COLLECTIONS SECTION*/
+const filterBtn = document.querySelectorAll("[data-filter-btn]");
+const filterItems = document.querySelectorAll("[data-filter]");
+
+let lastClickedBtn = filterBtn[0];
+
+const filter = function () {
+  lastClickedBtn.classList.remove("active");
+  this.classList.add("active");
+  lastClickedBtn = this;
+
+  for (let i = 0; i < filterItems.length; i++) {
+    if (filterItems[i].dataset.filter === this.dataset.filterBtn) {
+      filterItems[i].style.display = "block";
+    } else {
+      filterItems[i].style.display = "none";
+    }
+  }
+}
+
+addEventOnElem(filterBtn, "click", filter);
+
+
+/*when clicking on the Contact in navbar, scroll to the bottom of the document to view contact info*/
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+     e.preventDefault();
+
+     document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+     });
+  });
+});
+
+
+//dropdown on user ikonici
+/*function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}*/
